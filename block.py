744672a38f5ac3416a76255af4876f5ba1fd737e
Mateusz Joniak <mjoniak@gmail.com>
2018-02-07 11:56:20 +0100

    add labels to blocks

--- block.py
+++ block.py
@@ -10,11 +10,12 @@ class Block(Rectangle):
     WIDTH = 50
     HEIGHT = 50
 
-    def __init__(self, x, y, input_connectors_n, output_connectors_n, get_value=None):
+    def __init__(self, x, y, input_connectors_n, output_connectors_n, get_value=None, label=None):
         super().__init__(x, y, self.WIDTH, self.HEIGHT)
         self._add_input(input_connectors_n)
         self._add_output(output_connectors_n)
         self._get_value = get_value
+        self._label = label
 
     def _add_input(self, input_connectors_n: int):
         separator = self._calculate_separator(input_connectors_n)
@@ -56,7 +57,7 @@ class Block(Rectangle):
         pass
 
     def label(self):
-        pass
+        return self._label
 
 
 class SourceBlock(Block):
@@ -84,35 +85,36 @@ def source(value=1, x=0, y=0):
 
 
 def negation(x=0, y=0):
-    return Block(x, y, input_connectors_n=1, output_connectors_n=1, get_value=lambda inputs: not inputs[0].value)
+    return Block(x, y, input_connectors_n=1, output_connectors_n=1, get_value=lambda inputs: not inputs[0].value, label='NOT')
 
 
 def alternative(x=0, y=0):
-    return double_block(x, y, lambda a, b: a or b)
+    return double_block(x, y, lambda a, b: a or b, label='OR')
 
 
 def conjunction(x=0, y=0):
-    return double_block(x, y, lambda a, b: a and b)
+    return double_block(x, y, lambda a, b: a and b, label='AND')
 
 
 def nand(x=0, y=0):
-    return double_block(x, y, lambda a, b: not (a == 1 and b == 1))
+    return double_block(x, y, lambda a, b: not (a == 1 and b == 1), label='NAND')
 
 
 def nor(x=0, y=0):
-    return double_block(x, y, lambda a, b: not (a == 1 or b == 1))
+    return double_block(x, y, lambda a, b: not (a == 1 or b == 1), label='NOR')
 
 
 def xor(x=0, y=0):
-    return double_block(x, y, operator.xor)
+    return double_block(x, y, operator.xor, label='XOR')
 
 
 def display_block(x=0, y=0):
     return DisplayBlock(x, y)
 
 
-def double_block(x=0, y=0, get_value=lambda _, __: 0):
+def double_block(x=0, y=0, get_value=lambda _, __: 0, label=None):
     return Block(x, y, input_connectors_n=2, output_connectors_n=1,
+                 label=label,
                  get_value=lambda inputs: (
                      (inputs[0].value is not None
                       or inputs[1].value is not None)
